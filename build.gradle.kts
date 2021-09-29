d1d587b7851cc74c8c9e4be947b388e8c899e88d
pavlospt <p.tournaris@gmail.com>
2021-09-29 13:40:35 +0300

    Specify custom version

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ allprojects {
 }
 
 subprojects {
-    version = System.getenv("GITHUB_REF")?.substring(10) ?: System.getProperty("GITHUB_REF")?.substring(10) ?: "unknown"
+    version = System.getenv("GITHUB_REF")?.substring(10) ?: System.getProperty("GITHUB_REF")?.substring(10) ?: "0.4.0"
 
     plugins.withType<DokkaPlugin> {
         tasks.withType<DokkaTask>().configureEach {
