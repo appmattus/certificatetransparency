e794f611bf76aa1d0ad359d94f5ef9b42dac6fb8
Matthew Dolan <info@mattdolan.com>
2021-10-04 00:27:05 +0100

    Update copyright for new compose views

--- sampleapp/src/main/java/com/appmattus/certificatetransparency/sampleapp/MainActivity.kt
+++ sampleapp/src/main/java/com/appmattus/certificatetransparency/sampleapp/MainActivity.kt
@@ -1,6 +1,5 @@
 /*
  * Copyright 2021 Appmattus Limited
- * Copyright 2019 Babylon Partners Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,9 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * File modified by Appmattus Limited
- * See: https://github.com/appmattus/certificatetransparency/compare/e3d469df9be35bcbf0f564d32ca74af4e5ca4ae5...main
  */
 
 package com.appmattus.certificatetransparency.sampleapp
@@ -32,8 +28,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-
         setContent {
             val navController = rememberNavController()
             NavHost(navController, startDestination = "main") {
