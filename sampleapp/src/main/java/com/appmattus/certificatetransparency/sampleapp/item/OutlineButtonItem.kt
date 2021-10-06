95c2a8e7f9fb891e8a7f0bbb15f3f35f61c20b71
Matthew Dolan <info@mattdolan.com>
2021-10-06 09:22:25 +0100

    detekt tidy up

--- sampleapp/src/main/java/com/appmattus/certificatetransparency/sampleapp/item/OutlineButtonItem.kt
+++ sampleapp/src/main/java/com/appmattus/certificatetransparency/sampleapp/item/OutlineButtonItem.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.appmattus.certificatetransparency.sampleapp.R
-import java.util.Locale
 
 @Composable
 fun OutlineButtonItem(title: String, @DrawableRes icon: Int, onClick: () -> Unit, modifier: Modifier = Modifier) {
