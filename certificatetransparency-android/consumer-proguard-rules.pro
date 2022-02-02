6586d474aa96a6083b1e04f11b96699286921540
Matthew Dolan <info@mattdolan.com>
2022-02-02 08:55:37 +0000

    Add integration test for provider

--- certificatetransparency-android/consumer-proguard-rules.pro
+++ certificatetransparency-android/consumer-proguard-rules.pro
@@ -48,24 +48,32 @@
 
 ## Kotlinx Serialization
 
--keepattributes *Annotation*, InnerClasses
--dontnote kotlinx.serialization.AnnotationsKt # core serialization annotations
-
-# kotlinx-serialization-json specific. Add this if you have java.lang.NoClassDefFoundError kotlinx.serialization.json.JsonObjectSerializer
--keepclassmembers class kotlinx.serialization.json.** {
-    *** Companion;
+# Keep `Companion` object fields of serializable classes.
+# This avoids serializer lookup through `getDeclaredClasses` as done for named companion objects.
+-if @kotlinx.serialization.Serializable class **
+-keepclassmembers class <1> {
+    static <1>$Companion Companion;
 }
--keepclasseswithmembers class kotlinx.serialization.json.** {
+
+# Keep `serializer()` on companion objects (both default and named) of serializable classes.
+-if @kotlinx.serialization.Serializable class ** {
+    static **$* *;
+}
+-keepclassmembers class <1>$<3> {
     kotlinx.serialization.KSerializer serializer(...);
 }
 
--keep,includedescriptorclasses class com.appmattus.**$$serializer { *; }
--keepclassmembers class com.appmattus.** {
-    *** Companion;
+# Keep `INSTANCE.serializer()` of serializable objects.
+-if @kotlinx.serialization.Serializable class ** {
+    public static ** INSTANCE;
 }
--keepclasseswithmembers class com.appmattus.** {
+-keepclassmembers class <1> {
+    public static <1> INSTANCE;
     kotlinx.serialization.KSerializer serializer(...);
 }
+
+# @Serializable and @Polymorphic are used at runtime for polymorphic serialization.
+-keepattributes RuntimeVisibleAnnotations,AnnotationDefault
 
 
 ## Bouncycastle
---
+++ certificatetransparency/src/test/kotlin/com/appmattus/certificatetransparency/internal/CertificateTransparencyProviderIntegrationTest.kt
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2021 Appmattus Limited
+ * Copyright 2020 Babylon Partners Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * File modified by Appmattus Limited
+ * See: https://github.com/appmattus/certificatetransparency/compare/e3d469df9be35bcbf0f564d32ca74af4e5ca4ae5...main
+ */
+
+package com.appmattus.certificatetransparency.internal
+
+import com.appmattus.certificatetransparency.installCertificateTransparencyProvider
+import com.appmattus.certificatetransparency.removeCertificateTransparencyProvider
+import com.appmattus.certificatetransparency.utils.LogListDataSourceTestFactory
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.junit.After
+import org.junit.Test
+import javax.net.ssl.SSLHandshakeException
+
+internal class CertificateTransparencyProviderIntegrationTest {
+
+    companion object {
+        private const val invalidSctDomain = "no-sct.badssl.com"
+
+        private fun installProvider() {
+            installCertificateTransparencyProvider {
+                logListDataSource {
+                    LogListDataSourceTestFactory.logListDataSource
+                }
+            }
+        }
+
+        private fun installProviderAllowFails() {
+            installCertificateTransparencyProvider {
+                logListDataSource {
+                    LogListDataSourceTestFactory.logListDataSource
+                }
+
+                failOnError = false
+            }
+        }
+    }
+
+    @After
+    fun removeProvider() {
+        removeCertificateTransparencyProvider()
+    }
+
+    @Test
+    fun appmattusAllowed() {
+        installProvider()
+
+        val client = OkHttpClient.Builder().build()
+
+        val request = Request.Builder()
+            .url("https://www.appmattus.com")
+            .build()
+
+        client.newCall(request).execute()
+    }
+
+    @Test
+    fun insecureConnectionAllowed() {
+        installProvider()
+
+        val client = OkHttpClient.Builder().build()
+
+        val request = Request.Builder()
+            .url("http://www.appmattus.com")
+            .build()
+
+        client.newCall(request).execute()
+    }
+
+    @Test(expected = SSLHandshakeException::class)
+    fun invalidDisallowedWithException() {
+        installProvider()
+
+        val client = OkHttpClient.Builder().build()
+
+        val request = Request.Builder()
+            .url("https://$invalidSctDomain/")
+            .build()
+
+        client.newCall(request).execute()
+    }
+
+    @Test
+    fun invalidAllowedWhenFailsAllowed() {
+        installProviderAllowFails()
+
+        val client = OkHttpClient.Builder().build()
+
+        val request = Request.Builder()
+            .url("https://$invalidSctDomain/")
+            .build()
+
+        client.newCall(request).execute()
+    }
+
+    @Test
+    fun invalidAllowedWhenSctNotChecked() {
+        installCertificateTransparencyProvider {
+            -invalidSctDomain
+
+            logListDataSource {
+                LogListDataSourceTestFactory.logListDataSource
+            }
+        }
+
+        val client = OkHttpClient.Builder().build()
+
+        val request = Request.Builder()
+            .url("https://$invalidSctDomain/")
+            .build()
+
+        client.newCall(request).execute()
+    }
+
+    @Test(expected = SSLHandshakeException::class)
+    fun invalidNotAllowedWhenAllHostsIncluded() {
+        installCertificateTransparencyProvider {
+            logListDataSource {
+                LogListDataSourceTestFactory.logListDataSource
+            }
+        }
+
+        val client = OkHttpClient.Builder().build()
+
+        val request = Request.Builder()
+            .url("https://$invalidSctDomain/")
+            .build()
+
+        client.newCall(request).execute()
+    }
+}
