41d29172e9c566a5891e1b14ea6ac6811f4ace63
Matthew Dolan <info@mattdolan.com>
2023-02-25 06:58:27 +0000

    Replace use of bouncycastle in FileExt

--- certificatetransparency/src/test/kotlin/com/appmattus/certificatetransparency/utils/FileExt.kt
+++ certificatetransparency/src/test/kotlin/com/appmattus/certificatetransparency/utils/FileExt.kt
@@ -1,6 +1,5 @@
 /*
- * Copyright 2021 Appmattus Limited
- * Copyright 2019 Babylon Partners Limited
+ * Copyright 2023 Appmattus Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,17 +12,12 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * File modified by Appmattus Limited
- * See: https://github.com/appmattus/certificatetransparency/compare/e3d469df9be35bcbf0f564d32ca74af4e5ca4ae5...main
  */
 
 package com.appmattus.certificatetransparency.utils
 
 import com.appmattus.certificatetransparency.internal.utils.PublicKeyFactory
-import org.bouncycastle.util.io.pem.PemReader
 import java.io.File
-import java.io.FileReader
 import java.security.PublicKey
 
 /**
@@ -33,7 +27,5 @@ import java.security.PublicKey
  * @return [PublicKey] represented by this [File].
  */
 internal fun File.readPemFile(): PublicKey {
-    return PemReader(FileReader(this)).use {
-        PublicKeyFactory.fromByteArray(it.readPemObject().content)
-    }
+    return PublicKeyFactory.fromPemString(readText())
 }
