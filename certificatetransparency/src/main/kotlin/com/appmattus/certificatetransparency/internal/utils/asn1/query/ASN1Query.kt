ce7897c82c91fcd44990e87d9637a66ccaec931f
Matthew Dolan <info@mattdolan.com>
2023-04-24 13:42:41 +0100

    Pull commonName from certificate using ASN.1 Also added test to ensure we don't directly call any other non-Android SDKs in the future.

--- certificatetransparency/src/main/kotlin/com/appmattus/certificatetransparency/internal/utils/asn1/query/ASN1Query.kt
+++ certificatetransparency/src/main/kotlin/com/appmattus/certificatetransparency/internal/utils/asn1/query/ASN1Query.kt
@@ -18,6 +18,8 @@ package com.appmattus.certificatetransparency.internal.utils.asn1.query
 
 import com.appmattus.certificatetransparency.internal.utils.asn1.ASN1Object
 import com.appmattus.certificatetransparency.internal.utils.asn1.ASN1ObjectIdentifier
+import com.appmattus.certificatetransparency.internal.utils.asn1.ASN1PrintableStringTeletex
+import com.appmattus.certificatetransparency.internal.utils.asn1.ASN1PrintableStringUS
 import com.appmattus.certificatetransparency.internal.utils.asn1.ASN1Sequence
 
 internal data class ASN1Query(private val obj: ASN1Object) {
@@ -25,8 +27,22 @@ internal data class ASN1Query(private val obj: ASN1Object) {
         return ASN1Query((obj as ASN1Sequence).values[index])
     }
 
+    fun seqFirstOrNull(query: ASN1Query.() -> Boolean): ASN1Query? {
+        return (obj as ASN1Sequence).values.firstOrNull {
+            query(ASN1Query(it))
+        }?.let { ASN1Query(it) }
+    }
+
     fun oid(): String {
         return (obj as ASN1ObjectIdentifier).value
+    }
+
+    fun string(): String {
+        return when (obj) {
+            is ASN1PrintableStringTeletex -> obj.value
+            is ASN1PrintableStringUS -> obj.value
+            else -> error("Unsupported string object $obj")
+        }
     }
 }
 
