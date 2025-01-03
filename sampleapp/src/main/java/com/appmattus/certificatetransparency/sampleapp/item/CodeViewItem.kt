b002eaf5093c6486614652247772125bb4d37107
Matthew Dolan <info@mattdolan.com>
2025-01-03 08:37:31 +0000

    Remove sout from compose preview

--- sampleapp/src/main/java/com/appmattus/certificatetransparency/sampleapp/item/CodeViewItem.kt
+++ sampleapp/src/main/java/com/appmattus/certificatetransparency/sampleapp/item/CodeViewItem.kt
@@ -44,6 +44,5 @@ fun CodeViewItem(language: Language, sourceCode: String?, modifier: Modifier = M
 @Preview
 @Composable
 fun PreviewCodeViewItem() {
-    // mobsf-ignore: android_kotlin_logging
-    CodeViewItem(language = Language.JAVA, sourceCode = "fun main() {\n    System.out.println(\"Hello world!\");\n}")
+    CodeViewItem(language = Language.JAVA, sourceCode = "fun main() {\n    helloWorld();\n}")
 }
