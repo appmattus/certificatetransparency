ad910abf95daba131f4b9f8528fc759b08350dc5
Matthew Dolan <info@mattdolan.com>
2025-02-04 10:26:37 +0000

    Allow failOnError to be changed dynamically

--- certificatetransparency/src/main/kotlin/com/appmattus/certificatetransparency/internal/verifier/CertificateTransparencyInterceptor.kt
+++ certificatetransparency/src/main/kotlin/com/appmattus/certificatetransparency/internal/verifier/CertificateTransparencyInterceptor.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2023 Appmattus Limited
+ * Copyright 2021-2025 Appmattus Limited
  * Copyright 2019 Babylon Partners Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -45,7 +45,7 @@ internal class CertificateTransparencyInterceptor(
     logListDataSource: DataSource<LogListResult>?,
     policy: CTPolicy?,
     diskCache: DiskCache? = null,
-    private val failOnError: Boolean = true,
+    private val failOnError: () -> Boolean = { true },
     private val logger: CTLogger? = null
 ) : Interceptor, CertificateTransparencyBase(
     includeHosts,
@@ -74,7 +74,7 @@ internal class CertificateTransparencyInterceptor(
 
         logger?.log(host, result)
 
-        if (result is VerificationResult.Failure && failOnError) {
+        if (result is VerificationResult.Failure && failOnError()) {
             throw SSLPeerUnverifiedException("Certificate transparency failed")
         }
 
