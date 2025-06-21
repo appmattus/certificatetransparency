2975779a2064cd189e44ceaec0847a65fd529420
George Yammine <georgeyamminedev@gmail.com>
2025-06-21 10:18:53 -0700

    tiledLogs.isNotEmpty()

--- certificatetransparency/src/main/kotlin/com/appmattus/certificatetransparency/internal/loglist/model/v3/Operator.kt
+++ certificatetransparency/src/main/kotlin/com/appmattus/certificatetransparency/internal/loglist/model/v3/Operator.kt
@@ -27,16 +27,18 @@ import kotlinx.serialization.Serializable
  * @property name Name of this log operator
  * @property email CT log operator email addresses. The log operator can be contacted using any of these email addresses. (format: email)
  * @property logs Details of Certificate Transparency logs run by this operator.
+ * @property tiledLogs Details of tiled Certificate Transparency logs run by this operator.
  */
 @Serializable
 internal data class Operator(
     @SerialName("name") val name: String,
     @SerialName("email") val email: List<String>,
-    @SerialName("logs") val logs: List<Log>
+    @SerialName("logs") val logs: List<Log>,
+    @SerialName("tiled_logs") val tiledLogs: List<Log> = emptyList()
 ) {
     init {
         require(name.isNotEmpty())
         require(email.isNotEmpty())
-        require(logs.isNotEmpty())
+        require(logs.isNotEmpty() || tiledLogs.isNotEmpty())
     }
 }
